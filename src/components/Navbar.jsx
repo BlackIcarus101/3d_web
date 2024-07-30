21a77d595e70d3ee112dcaa58c20a3f4f9c7a3e8
Ygue Bellande <ywbellande@gmail.com>
2024-07-30 01:05:03 -0400

    New Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu } from '../assets';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -14,7 +14,8 @@ const Navbar = () => {
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/" className='flex items-center gap-2' onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Ygue Bellande <span className='sm:block hidden'>| DevOps Engineer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Ygue Bellande &nbsp;
+             <span className='sm:block hidden'>| DevOps Engineer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
